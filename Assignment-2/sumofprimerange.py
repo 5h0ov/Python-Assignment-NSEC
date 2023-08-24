e807d0cd7f25897e2c709cd69e31377d431f6b5c
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2023-08-24 17:19:23 +0530

    Update sumofprimerange.py

--- Assignment-2/sumofprimerange.py
+++ Assignment-2/sumofprimerange.py
@@ -1,13 +1,20 @@
-n=(int)(input("Enter value of n for the range 1 to n: "))
-sum=0
+print("Example range of numbers: a to b")
+a=(int)(input("Enter value of a: "))
+b=(int)(input("Enter value of b: "))
+sum=0   
+prime=True
 
-for i in range(2,n):
+for i in range(a,b):
     if(i>1):
         prime= True
+    if(i==1):
+        prime= False
+        
     for j in range(2,int(i ** 0.5)+1):
         if(i%j==0):
             prime= False
             break
-    sum+=i
+    if(prime):
+        sum+=i
     
 print("The sum of all the prime numbers in that given range is",sum)
