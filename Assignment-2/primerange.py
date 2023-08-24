329b04757a38876827e039b15803cc549d1c2f3d
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2023-08-24 17:19:34 +0530

    Update primerange.py

--- Assignment-2/primerange.py
+++ Assignment-2/primerange.py
@@ -7,6 +7,9 @@ prime= False
 for i in range(a,b):
     if(i>1):
         prime= True
+    if(i==1):
+        prime= False
+        
     for j in range(2,int(i ** 0.5)+1):
         if(i%j==0):
             prime= False
